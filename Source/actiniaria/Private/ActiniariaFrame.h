175bd8c86f1a9d30252990e4ddd166ab769e130f
d9 <nustxujun@gmail.com>
2019-12-17 01:13:42 +0800

    fps

--- Source/actiniaria/Private/ActiniariaFrame.h
+++ Source/actiniaria/Private/ActiniariaFrame.h
@@ -118,20 +118,22 @@ public:
 	void updateImpl()
 	{
 		static auto lastTime = GetTickCount64();
+		static auto framecount = 0;
 		auto cur = GetTickCount64();
 		auto delta = cur - lastTime;
+		framecount++;
 		if (delta > 0)
 		{
 			lastTime = cur;
-
-			float time = 1000.0f / delta;
+			float time = (float)framecount * 1000.0f / (float)delta;
+			framecount = 0;
 			static float history = time;
-
 			history = history * 0.99f + time * 0.01f;
 
 			std::stringstream ss;
 			ss.precision(4);
-			ss << history;
+			ss << history << "(" << 1000.0f / history << "ms)";
+			::SetWindowTextA(Renderer::getSingleton()->getWindow(), ss.str().c_str());
 		}
 
 		pipeline->update();
